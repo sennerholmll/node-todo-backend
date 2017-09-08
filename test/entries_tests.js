0231286cedf5ea1bdef3dba838c04d9460f632af
Mikael Hallendal <hallski@hallski.org>
2017-09-08 22:31:35 +0200

    Add test to check for error from a router
    Figured out a way to handle error replies from the router when testing
    with async/await.

--- test/entries_tests.js
+++ test/entries_tests.js
@@ -8,6 +8,14 @@ const express = require('express')
 const memstore = require('../database/memstore')
 const entries = require('../app/routes/entries')
 
+function allowErrorResponse(allowedErrors, promise) {
+  return promise.catch(err => {
+    if (allowedErrors.includes(err.status)) {
+      return err.response
+    }
+    throw err
+  })
+}
 
 describe('Entries', () => {
   var store
@@ -19,7 +27,7 @@ describe('Entries', () => {
     app.use(entries.create_router(store))
   })
 
-  it('/ for empty database should return an empty array', async function() {
+  it('get / for empty database should return an empty array', async function() {
     res = await chai.request(app).get('/')
 
     expect(res).to.be.json
@@ -27,7 +35,7 @@ describe('Entries', () => {
     expect(res.body).to.have.lengthOf(0)
   })
 
-  it('/ should list all entries in database', async function() {
+  it('get / should list all entries in database', async function() {
     await store.collection('Entry').save({ name: 'a name' })
     await store.collection('Entry').save({ name: 'another one' })
 
@@ -36,5 +44,12 @@ describe('Entries', () => {
     expect(res).to.be.json
     expect(res).to.have.status(200)
     expect(res.body).to.be.eql(Array.from(await store.collection('Entry').find()))
+  })
+
+ it('post / gives 422 on invalid entry', async function() {
+    const res = await allowErrorResponse([422], chai.request(app).post('/').send({ name: 'name' }))
+
+    expect(res).to.have.status(422)
+    expect(res.text).to.not.be.undefined
   })
 })
