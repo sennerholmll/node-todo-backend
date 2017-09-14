877a7357aa2d977ba9abee181b0215e89b016c8f
Mikael Hallendal <mikael.hallendal@cag.se>
2017-09-14 16:46:30 +0200

    Fetch image before running

--- scripts/run-system-test.sh
+++ scripts/run-system-test.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
 set -e
-
+set -x
 imagename=${IMAGENAME:-todo}
 version=${GO_PIPELINE_LABEL:-dev}
 
 repository_prefix=${REPOSITORY_PREFIX}
+
+full_imagename=${repository_prefix}/${imagename}:${version}
 
 mode="--fast"
 if [ $# -eq 1 ]; then
     mode=$1
 fi
 
+# Fetch the image
+gcloud docker -- pull ${full_imagename}
+
 # Instant
 if [ "${mode}" = "--fast" ]; then
-#    docker run --rm --mount type=bind,src=${GOOGLE_APPLICATION_CREDENTIALS},dst=${GOOGLE_APPLICATION_CREDENTIALS} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} ${imagename}:${version} ./runtest --fast
-    docker run --rm -v ${GOOGLE_APPLICATION_CREDENTIALS}:${GOOGLE_APPLICATION_CREDENTIALS} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} ${repository_prefix}/${imagename}:${version} ./runtest --fast
+#    docker run --rm --mount type=bind,src=${GOOGLE_APPLICATION_CREDENTIALS},dst=${GOOGLE_APPLICATION_CREDENTIALS} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} ${full_imagename} ./runtest --fast
+    docker run --rm -v ${GOOGLE_APPLICATION_CREDENTIALS}:${GOOGLE_APPLICATION_CREDENTIALS} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} ${full_imagename} ./runtest --fast
 fi
