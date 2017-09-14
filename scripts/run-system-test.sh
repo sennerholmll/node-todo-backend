c99389da7cc3b383faa9955d0974f6b60a3fef73
Mikael Hallendal <mikael.hallendal@cag.se>
2017-09-14 16:19:29 +0200

    Attempt to fix script for older docker

--- scripts/run-system-test.sh
+++ scripts/run-system-test.sh
@@ -12,5 +12,6 @@ fi
 
 # Instant
 if [ "${mode}" = "--fast" ]; then
-    docker run --rm --mount type=bind,src=${GOOGLE_APPLICATION_CREDENTIALS},dst=${GOOGLE_APPLICATION_CREDENTIALS} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} ${imagename}:${version} ./runtest --fast
+#    docker run --rm --mount type=bind,src=${GOOGLE_APPLICATION_CREDENTIALS},dst=${GOOGLE_APPLICATION_CREDENTIALS} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} ${imagename}:${version} ./runtest --fast
+    docker run --rm -v ${GOOGLE_APPLICATION_CREDENTIALS}:${GOOGLE_APPLICATION_CREDENTIALS} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} ${imagename}:${version} ./runtest --fast
 fi
