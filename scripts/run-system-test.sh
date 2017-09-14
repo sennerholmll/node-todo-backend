7ca6b0a0ca76d0f5b57fa51dd07d6185adc7fc07
Mikael Hallendal <mikael.hallendal@cag.se>
2017-09-14 16:26:16 +0200

    Fix docker image name

--- scripts/run-system-test.sh
+++ scripts/run-system-test.sh
@@ -5,6 +5,8 @@ set -e
 imagename=${IMAGENAME:-todo}
 version=${GO_PIPELINE_LABEL:-dev}
 
+repository_prefix=${REPOSITORY_PREFIX}
+
 mode="--fast"
 if [ $# -eq 1 ]; then
     mode=$1
@@ -13,5 +15,5 @@ fi
 # Instant
 if [ "${mode}" = "--fast" ]; then
 #    docker run --rm --mount type=bind,src=${GOOGLE_APPLICATION_CREDENTIALS},dst=${GOOGLE_APPLICATION_CREDENTIALS} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} ${imagename}:${version} ./runtest --fast
-    docker run --rm -v ${GOOGLE_APPLICATION_CREDENTIALS}:${GOOGLE_APPLICATION_CREDENTIALS} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} ${imagename}:${version} ./runtest --fast
+    docker run --rm -v ${GOOGLE_APPLICATION_CREDENTIALS}:${GOOGLE_APPLICATION_CREDENTIALS} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} ${repository_prefix}/${imagename}:${version} ./runtest --fast
 fi
