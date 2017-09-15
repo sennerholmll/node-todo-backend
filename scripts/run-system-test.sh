68c379d17fa00d3d7c6017c325efb912200364a2
Mikael Sennerholm <mikael@sennerholm.net>
2017-09-15 16:39:51 +0200

    Wrong variable name

--- scripts/run-system-test.sh
+++ scripts/run-system-test.sh
@@ -22,6 +22,6 @@ if [ "${mode}" = "--fast" ]; then
 # Hardcoded parts to werk on a Go agent running in docker cloud
     docker_id=`docker ps | grep $HOSTNAME | grep -v POD | awk '{print $1}'`
     cred_dir=`dirname ${GOOGLE_APPLICATION_CREDENTIALS}`
-    host_vol_dir=`docker inspect  $dockerid  | grep $cred_dir | grep /var/lib/kub | sed s/\"//g | awk -F: '{print $1}'`
+    host_vol_dir=`docker inspect  $docker_id  | grep $cred_dir | grep /var/lib/kub | sed s/\"//g | awk -F: '{print $1}'`
     docker run --rm -v ${host_vol_dir}:${cred_dir} -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} ${full_imagename} ./runtest --fast
 fi
