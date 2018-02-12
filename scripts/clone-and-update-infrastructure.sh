1a520ae080bec0c8c699044031ddd295a0742c88
Circle CI <mikael+circleci@sennerholm.net>
2018-02-12 22:51:15 +0100

    More dynamic handling of different repos

--- scripts/clone-and-update-infrastructure.sh
+++ scripts/clone-and-update-infrastructure.sh
@@ -7,8 +7,9 @@ source ${directory}/common.sh
 
 gitref=${1:-noref}
 version=${2:-latest}
-path=${3:-terraform-infrastructure-live/gce_account/europe-west1/fast/todo-backend-fast}
-infrarepo=${4:-git@github.com:sennerholm/terraform-infrastructure-live.git}
+path=${3:-terraform-infrastructure-live/gce_account/europe-west1/fast/todo-backend}
+infrarepo=${4:-`echo $CIRCLE_REPOSITORY_URL | awk -F'/' '{print $1}'`/terraform-infrastructure-live.git}
+githubaccount=${5:-`echo $CIRCLE_REPOSITORY_URL | awk -F'/' '{print $1}' | awk -F':' '{print $2}'`}
 
 # Add github ssh key
 if [ ! -d ~/.ssh ]; then
@@ -19,7 +20,8 @@ git clone ${infrarepo}
 cd ${path}
 echo version = ${version} > version.tfvars
 sed -i s/?ref=.*\"/?ref=${gitref}\"/ terraform.tfvars
-
+sed -i s/github.com\\/[^\\/]*\\//github.com\\/${githubaccount}\\// terraform.tfvars
+git diff -u
 git config --global user.email "mikael+circleci@sennerholm.net"
 git config --global user.name "Circle CI"
 git commit -m"Automated Updated ${path} to image version ${version} and gitref ${gitref}" -a
